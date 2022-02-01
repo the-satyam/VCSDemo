113a7de9c4d5e1b8dbb20f8a6488e304f2e939e2
Nimeshikasri <nimeshikasri.k@gmail.com>
2022-02-01 14:23:05 +0530

    added splash activity

---
+++ app/src/main/java/com/example/vcsdemo/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vcsdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
